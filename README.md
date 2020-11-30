6d13b3cd6a8fccee417ade804b1874f037b4bf20
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-11-30 11:33:03 +0200

    feat: add example 02

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ lets explore how to build a graphql based api and its many tools and options, ba
 
 - `01-basic` - just a basic apollo & express integration to "get it going" 🚀
 
-- `02-graphql-code-generator` - just a basic apollo & express integration to "get it going" 🚀
+- `02-graphql-code-generator` - a neat tool to generate usefull types and methods for a given gql endpoint. for this example we usd the (https://countries.trevorblades.com) endpoint to generate the content of `src/types.d.ts`
--- src/02-graphql-code-generator/src/types.d.ts
+++ src/02-graphql-code-generator/src/types.d.ts
@@ -1,7 +1,9 @@
+import { GraphQLResolveInfo, GraphQLScalarType, GraphQLScalarTypeConfig } from 'graphql';
 export type Maybe<T> = T | null;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type RequireFields<T, K extends keyof T> = { [X in Exclude<keyof T, K>]?: T[X] } & { [P in K]-?: NonNullable<T[P]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -9,9 +11,289 @@ export type Scalars = {
   Boolean: boolean;
   Int: number;
   Float: number;
+  /** The `Upload` scalar type represents a file upload. */
+  Upload: any;
 };
 
 export type Query = {
   __typename?: 'Query';
-  hello?: Maybe<Scalars['String']>;
+  continents: Array<Continent>;
+  continent?: Maybe<Continent>;
+  countries: Array<Country>;
+  country?: Maybe<Country>;
+  languages: Array<Language>;
+  language?: Maybe<Language>;
 };
+
+
+export type QueryContinentsArgs = {
+  filter?: Maybe<ContinentFilterInput>;
+};
+
+
+export type QueryContinentArgs = {
+  code: Scalars['ID'];
+};
+
+
+export type QueryCountriesArgs = {
+  filter?: Maybe<CountryFilterInput>;
+};
+
+
+export type QueryCountryArgs = {
+  code: Scalars['ID'];
+};
+
+
+export type QueryLanguagesArgs = {
+  filter?: Maybe<LanguageFilterInput>;
+};
+
+
+export type QueryLanguageArgs = {
+  code: Scalars['ID'];
+};
+
+export type ContinentFilterInput = {
+  code?: Maybe<StringQueryOperatorInput>;
+};
+
+export type StringQueryOperatorInput = {
+  eq?: Maybe<Scalars['String']>;
+  ne?: Maybe<Scalars['String']>;
+  in?: Maybe<Array<Maybe<Scalars['String']>>>;
+  nin?: Maybe<Array<Maybe<Scalars['String']>>>;
+  regex?: Maybe<Scalars['String']>;
+  glob?: Maybe<Scalars['String']>;
+};
+
+export type Continent = {
+  __typename?: 'Continent';
+  code: Scalars['ID'];
+  name: Scalars['String'];
+  countries: Array<Country>;
+};
+
+export type Country = {
+  __typename?: 'Country';
+  code: Scalars['ID'];
+  name: Scalars['String'];
+  native: Scalars['String'];
+  phone: Scalars['String'];
+  continent: Continent;
+  capital?: Maybe<Scalars['String']>;
+  currency?: Maybe<Scalars['String']>;
+  languages: Array<Language>;
+  emoji: Scalars['String'];
+  emojiU: Scalars['String'];
+  states: Array<State>;
+};
+
+export type Language = {
+  __typename?: 'Language';
+  code: Scalars['ID'];
+  name?: Maybe<Scalars['String']>;
+  native?: Maybe<Scalars['String']>;
+  rtl: Scalars['Boolean'];
+};
+
+export type State = {
+  __typename?: 'State';
+  code?: Maybe<Scalars['String']>;
+  name: Scalars['String'];
+  country: Country;
+};
+
+export type CountryFilterInput = {
+  code?: Maybe<StringQueryOperatorInput>;
+  currency?: Maybe<StringQueryOperatorInput>;
+  continent?: Maybe<StringQueryOperatorInput>;
+};
+
+export type LanguageFilterInput = {
+  code?: Maybe<StringQueryOperatorInput>;
+};
+
+export enum CacheControlScope {
+  Public = 'PUBLIC',
+  Private = 'PRIVATE'
+}
+
+
+
+
+export type ResolverTypeWrapper<T> = Promise<T> | T;
+
+
+export type LegacyStitchingResolver<TResult, TParent, TContext, TArgs> = {
+  fragment: string;
+  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+};
+
+export type NewStitchingResolver<TResult, TParent, TContext, TArgs> = {
+  selectionSet: string;
+  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+};
+export type StitchingResolver<TResult, TParent, TContext, TArgs> = LegacyStitchingResolver<TResult, TParent, TContext, TArgs> | NewStitchingResolver<TResult, TParent, TContext, TArgs>;
+export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> =
+  | ResolverFn<TResult, TParent, TContext, TArgs>
+  | StitchingResolver<TResult, TParent, TContext, TArgs>;
+
+export type ResolverFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Promise<TResult> | TResult;
+
+export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => AsyncIterator<TResult> | Promise<AsyncIterator<TResult>>;
+
+export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
+  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
+}
+
+export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<any, TParent, TContext, TArgs>;
+  resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
+}
+
+export type SubscriptionObject<TResult, TKey extends string, TParent, TContext, TArgs> =
+  | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
+  | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
+
+export type SubscriptionResolver<TResult, TKey extends string, TParent = {}, TContext = {}, TArgs = {}> =
+  | ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
+  | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
+
+export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
+  parent: TParent,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
+
+export type IsTypeOfResolverFn<T = {}, TContext = {}> = (obj: T, context: TContext, info: GraphQLResolveInfo) => boolean | Promise<boolean>;
+
+export type NextResolverFn<T> = () => Promise<T>;
+
+export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs = {}> = (
+  next: NextResolverFn<TResult>,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+/** Mapping between all available schema types and the resolvers types */
+export type ResolversTypes = {
+  Query: ResolverTypeWrapper<{}>;
+  ID: ResolverTypeWrapper<Scalars['ID']>;
+  ContinentFilterInput: ContinentFilterInput;
+  StringQueryOperatorInput: StringQueryOperatorInput;
+  String: ResolverTypeWrapper<Scalars['String']>;
+  Continent: ResolverTypeWrapper<Continent>;
+  Country: ResolverTypeWrapper<Country>;
+  Language: ResolverTypeWrapper<Language>;
+  Boolean: ResolverTypeWrapper<Scalars['Boolean']>;
+  State: ResolverTypeWrapper<State>;
+  CountryFilterInput: CountryFilterInput;
+  LanguageFilterInput: LanguageFilterInput;
+  CacheControlScope: CacheControlScope;
+  Upload: ResolverTypeWrapper<Scalars['Upload']>;
+};
+
+/** Mapping between all available schema types and the resolvers parents */
+export type ResolversParentTypes = {
+  Query: {};
+  ID: Scalars['ID'];
+  ContinentFilterInput: ContinentFilterInput;
+  StringQueryOperatorInput: StringQueryOperatorInput;
+  String: Scalars['String'];
+  Continent: Continent;
+  Country: Country;
+  Language: Language;
+  Boolean: Scalars['Boolean'];
+  State: State;
+  CountryFilterInput: CountryFilterInput;
+  LanguageFilterInput: LanguageFilterInput;
+  Upload: Scalars['Upload'];
+};
+
+export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
+  continents?: Resolver<Array<ResolversTypes['Continent']>, ParentType, ContextType, RequireFields<QueryContinentsArgs, never>>;
+  continent?: Resolver<Maybe<ResolversTypes['Continent']>, ParentType, ContextType, RequireFields<QueryContinentArgs, 'code'>>;
+  countries?: Resolver<Array<ResolversTypes['Country']>, ParentType, ContextType, RequireFields<QueryCountriesArgs, never>>;
+  country?: Resolver<Maybe<ResolversTypes['Country']>, ParentType, ContextType, RequireFields<QueryCountryArgs, 'code'>>;
+  languages?: Resolver<Array<ResolversTypes['Language']>, ParentType, ContextType, RequireFields<QueryLanguagesArgs, never>>;
+  language?: Resolver<Maybe<ResolversTypes['Language']>, ParentType, ContextType, RequireFields<QueryLanguageArgs, 'code'>>;
+};
+
+export type ContinentResolvers<ContextType = any, ParentType extends ResolversParentTypes['Continent'] = ResolversParentTypes['Continent']> = {
+  code?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  countries?: Resolver<Array<ResolversTypes['Country']>, ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type CountryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Country'] = ResolversParentTypes['Country']> = {
+  code?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  native?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  phone?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  continent?: Resolver<ResolversTypes['Continent'], ParentType, ContextType>;
+  capital?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  currency?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  languages?: Resolver<Array<ResolversTypes['Language']>, ParentType, ContextType>;
+  emoji?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  emojiU?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  states?: Resolver<Array<ResolversTypes['State']>, ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type LanguageResolvers<ContextType = any, ParentType extends ResolversParentTypes['Language'] = ResolversParentTypes['Language']> = {
+  code?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  name?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  native?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  rtl?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type StateResolvers<ContextType = any, ParentType extends ResolversParentTypes['State'] = ResolversParentTypes['State']> = {
+  code?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  country?: Resolver<ResolversTypes['Country'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export interface UploadScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes['Upload'], any> {
+  name: 'Upload';
+}
+
+export type Resolvers<ContextType = any> = {
+  Query?: QueryResolvers<ContextType>;
+  Continent?: ContinentResolvers<ContextType>;
+  Country?: CountryResolvers<ContextType>;
+  Language?: LanguageResolvers<ContextType>;
+  State?: StateResolvers<ContextType>;
+  Upload?: GraphQLScalarType;
+};
+
+
+/**
+ * @deprecated
+ * Use "Resolvers" root object instead. If you wish to get "IResolvers", add "typesPrefix: I" to your config.
+ */
+export type IResolvers<ContextType = any> = Resolvers<ContextType>;
